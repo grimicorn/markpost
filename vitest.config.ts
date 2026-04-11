9362db634e66bcacdd602f6b4d161bd5b0dc72b5
Danny Holloran <dtholloran@gmail.com>
2026-04-11 10:33:33 -0700

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 export default defineConfig({
   resolve: {
     alias: {
+      "@": resolve(__dirname, "./src"),
       "@libs": resolve(__dirname, "./src/libs"),
       "@t": resolve(__dirname, "./src/types"),
       "@fns": resolve(__dirname, "./src/functions"),
